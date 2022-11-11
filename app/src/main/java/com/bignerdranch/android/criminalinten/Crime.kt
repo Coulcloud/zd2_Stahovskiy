8a89d24f377cecbbc54e380544d1638d1135fbd8
Coulcloud <ultrasoykb12@mail.ru>
2022-11-11 21:20:48 +0500

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalinten/Crime.kt
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.criminalinten
+
+import java.util.*
+
+data class Crime (var id:UUID = UUID.randomUUID(),
+                  var date: Int = 0,
+                  var title: String = "",
+                  var isSolved: Boolean = false)
+{
+constructor(id: UUID, title: String, date: Int, isSolved: Boolean):this(id)
+{
+    this.title=title
+    this.date=date
+    this.id=id
+    this.isSolved=isSolved
+}
+
+
+}
